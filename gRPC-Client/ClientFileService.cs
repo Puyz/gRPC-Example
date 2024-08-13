6edcfff5521ab7493578e9e08197abf4447f7e21
Ömer temel <42616536+Puyz@users.noreply.github.com>
2024-08-13 19:17:48 +0300

    file transfer

---
+++ gRPC-Client/ClientFileService.cs
@@ -0,0 +1,76 @@
+﻿using Google.Protobuf;
+using Grpc.Net.Client;
+using grpcFileTransportClient;
+
+namespace gRPC_Client
+{
+    public class ClientFileService
+    {
+        private readonly GrpcChannel _channel;
+        private readonly FileService.FileServiceClient _fileServiceClient;
+        public ClientFileService()
+        {
+            _channel = GrpcChannel.ForAddress("https://localhost:7205");
+            _fileServiceClient = new(_channel);
+        }
+
+        public async Task Download(string fullFileName)
+        {
+            string downloadPath = @"DownloadPath\";
+
+            grpcFileTransportClient.FileInfo fileInfo = new()
+            {
+                FileName = Path.GetFileNameWithoutExtension(fullFileName),
+                FileExtension = Path.GetExtension(fullFileName)
+            };
+
+            FileStream fileStream = null;
+
+            var download = _fileServiceClient.FileDownload(fileInfo);
+            int count = 0;
+            decimal chunkSize = 0;
+            CancellationTokenSource tokenSource = new();
+            while (await download.ResponseStream.MoveNext(tokenSource.Token))
+            {
+                if (count++ == 0)
+                {
+                    fileStream = new FileStream($"{downloadPath}/{download.ResponseStream.Current.Info.FileName}{download.ResponseStream.Current.Info.FileExtension}", FileMode.CreateNew);
+                    fileStream.SetLength(download.ResponseStream.Current.FileSize);
+                }
+
+                var buffer = download.ResponseStream.Current.Buffer.ToByteArray();
+                await fileStream.WriteAsync(buffer, 0, download.ResponseStream.Current.ReadedByte);
+                Console.Write("\r{0:N2}%", (chunkSize += download.ResponseStream.Current.ReadedByte) * 100 / download.ResponseStream.Current.FileSize);
+            }
+
+            Console.WriteLine("Başarıyla indirildi.");
+            await fileStream.DisposeAsync();
+            fileStream.Close();
+        }
+        public async Task Upload(string file)
+        {
+            using FileStream fileStream = new(file, FileMode.Open);
+
+            var content = new BytesContent
+            {
+                FileSize = fileStream.Length,
+                Info = new grpcFileTransportClient.FileInfo { FileName = Path.GetFileNameWithoutExtension(file), FileExtension = Path.GetExtension(file) },
+                ReadedByte = 0
+            };
+
+            var upload = _fileServiceClient.FileUpload();
+            byte[] buffer = new byte[2048];
+
+            while ((content.ReadedByte = await fileStream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+            {
+                content.Buffer = ByteString.CopyFrom(buffer);
+                await upload.RequestStream.WriteAsync(content);
+            }
+
+            await upload.RequestStream.CompleteAsync();
+            fileStream.Close();
+        }
+
+
+    }
+}
--- gRPC-Client/Program.cs
+++ gRPC-Client/Program.cs
@@ -1,94 +1,17 @@
-﻿using Grpc.Net.Client;
-using gRPC_Message_Client;
-
-var channel = GrpcChannel.ForAddress("https://localhost:7205/");
-
-//var greetClient = new Greeter.GreeterClient(channel);
-//HelloReply reply = await greetClient.SayHelloAsync(new HelloRequest { Name = "Puyz" });
-
-var messageClient = new Message.MessageClient(channel);
-
-/* Unary
- 
-var response = await messageClient.SendMessageAsync(new MessageRequest { 
-    Username = "Puyz",
-    Message = "Merhaba"
-});
-Console.WriteLine(response.Message);
-*/
-
-/* Server Streaming
- 
-MessageResponse response = messageClient.SendMessage(new MessageRequest
-{
-    Username = "Puyz",
-    Message = "Merhaba"
-});
-
-CancellationTokenSource tokenSource = new();
-
-while (await response.ResponseStream.MoveNext(tokenSource.Token))
-{
-    Console.WriteLine(response.ResponseStream.Current.Message);
-}
-*/
-
-/* Client Streaming
- 
-var request = messageClient.SendMessage();
-for (int i = 0; i < 5; i++)
-{
-    await Task.Delay(1000);
-    await request.RequestStream.WriteAsync(new MessageRequest
-    {
-        Username = "Ömer",
-        Message = $"{i}. Mesaj"
-    });
-}
-
-// Stream data'nın sonlandığını bildiriyoruz.
-await request.RequestStream.CompleteAsync();
-
-Console.WriteLine((await request.ResponseAsync).Message);
-*/
-
-// Bi-directional Streaming
-
-var request = messageClient.SendMessage();
-var taskRequest = Task.Run(async () =>
-{
-    for (int i = 0; i < 10; i++)
-    {
-        await Task.Delay(1000);
-        await request.RequestStream.WriteAsync(new MessageRequest
-        {
-            Username = "Ömer",
-            Message = $"{i}. Mesaj"
-        });
-    }
-    
-});
-
-
-CancellationTokenSource cancellationToken = new();
-
-while (await request.ResponseStream.MoveNext(cancellationToken.Token))
-{
-    Console.WriteLine(request.ResponseStream.Current.Message);
-}
-
-await taskRequest;
-await request.RequestStream.CompleteAsync();
+﻿using gRPC_Client;
 
 
 
+ClientFileService fileService = new();
 
 
+// Upload
+string file = @"filePath/file_name.extension";
+await fileService.Upload(file);
 
 
-
-
-
-
+// Download
+await fileService.Download("file_name.extension");
 
 Console.ReadLine();
+
